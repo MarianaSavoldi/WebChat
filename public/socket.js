8b631f71c8f6ab6fba70b7e71e7cdcf1f5f22c9c
Mariana Savoldi <marianasavodi123@gmail.com>
2021-11-24 22:26:28 -0300

    feat: let all customers know who is online now

--- public/socket.js
+++ public/socket.js
@@ -1,23 +1,28 @@
 const socket = window.io();
 
+const dataTestId = 'data-testid';
+
 const form = document.getElementById('form');
 const inputMessage = document.getElementById('messageInput');
-const onlineUser = document.getElementById('nickname-box');
+const userNickname = document.getElementById('nickname-box');
 const buttonNick = document.getElementById('nicknameButton');
+let nick;
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (inputMessage.value) {
-    socket.emit('message', { chatMessage: inputMessage.value, nickname: socket.id.slice(0, 16) });
+    socket.emit('message', { chatMessage: inputMessage.value, nickname: nick });
     inputMessage.value = '';
   }
 });
 
 buttonNick.addEventListener('click', (e) => {
   e.preventDefault();
-  const nickUser = { nickname: (socket.id = onlineUser.value) };
-  onlineUser.value = '';
-  return nickUser;
+  nick = userNickname.value;
+  socket.emit('changeNickname', { nickname: nick, id: socket.id });
+  const userOnline = document.getElementById('online-now');
+  userOnline.innerHTML = nick;
+  userNickname.value = '';
 });
 
 const createMessage = (message) => {
@@ -25,25 +30,47 @@ const createMessage = (message) => {
   const chatLi = document.createElement('li');
   chatLi.innerHTML = message;
   // https://developer.mozilla.org/pt-BR/docs/Web/API/Element/setAttribute
-  chatLi.setAttribute('data-testid', 'message');
+  chatLi.setAttribute(dataTestId, 'message');
   chatUl.appendChild(chatLi);
 };
 
 const msgHistory = (history) => {
-  console.log('history', history);
   history.forEach(({ time, nickname, chatMessage }) => {
     const message = document.getElementById('chat');
-    console.log('messages', message);
     const li = document.createElement('li');
     li.innerText = `${time} - ${nickname} : ${chatMessage}`;
-    li.setAttribute('data-testid', 'message');
+    li.setAttribute(dataTestId, 'message');
     message.appendChild(li);
   });
 };
 
+const onlineUser = (nickname) => {
+  const onlineUl = document.getElementById('userNickname');
+  const onlineLi = document.createElement('li');
+  onlineLi.innerHTML = nickname;
+  onlineLi.setAttribute(dataTestId, 'online-user');
+  onlineLi.setAttribute('id', 'online-now');
+  onlineUl.appendChild(onlineLi);
+};
+/* 
+zambelli 0
+mari 1
+maiara 2
+*/
+const usersList = (users) => {
+  const onlineUsers = document.getElementById('userNickname');
+  onlineUsers.innerHTML = '';
+  const index = users.findIndex((user) => socket.id === user.id);
+  onlineUser(users[index].nickname);
+  for (let i = 0; i < users.length; i += 1) {
+    if (index !== i) {
+      onlineUser(users[i].nickname);
+      console.log('index !== i');
+    }
+  }
+};
+
 socket.on('history', (history) => msgHistory(history));
 socket.on('message', (message) => createMessage(message));
-socket.on('online', (user) => {
-  const pUser = document.getElementById('userNickname');
-  pUser.innerHTML = user;
-});
+socket.on('user', (users) => usersList(users));
+socket.on('connect', () => { nick = socket.id.slice(0, 16); });
--- server.js
+++ server.js
@@ -19,30 +19,41 @@ const io = require('socket.io')(http, {
 
 const msgModel = require('./models/msgHistory');
 
+let users = [];
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.get('/', (_req, res) => {
-  res.status(OK).render('index');
+  res.status(OK).render('index', { users });
 });
 
-io.on('connection', async (socket) => {
-  console.log('logged in user', socket.id);
-  socket.emit('online', socket.id.slice(0, 16));
-  socket.emit('history', await msgModel.history());
+const changeNickname = (nickname, id) => (user) => { 
+  if (id === user.id) return { ...user, nickname };
+  return user;
+};
 
+io.on('connection', async (socket) => {
+  const userNick = socket.id.slice(0, 16);
+  users.push({ nickname: userNick, id: socket.id });
+  io.emit('user', users);
+  socket.emit('history', await msgModel.history());
+  socket.on('changeNickname', ({ nickname, id }) => {
+  users = users.map(changeNickname(nickname, id));
+    io.emit('user', users);
+  });
   socket.on('message', async ({ chatMessage, nickname }) => {
-    console.log('Your nickname:', nickname);
     const time = new Date().toLocaleString().replace(/\//g, '-');
     await msgModel.create({ time, nickname, chatMessage });
     io.emit('message', `${time} - ${nickname}: ${chatMessage}`);
   });
-
   socket.on('disconnect', () => {
-    console.log('disconnected user', socket.id);
+    // https://www.mundojs.com.br/2018/09/06/removendo-elementos-de-uma-lista-array-javascript/
+    const index = users.findIndex((user) => socket.id === user.id);
+    users.splice(index, 1);
+    io.emit('user', users);
   });
 });
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 http.listen(port, (console.log(`Webchat open on port ${port}`)));
